95487db01d06e5ee3b23b508ccacacfec6a852be
sgykim15 <sgykim36@gmail.com>
2019-12-12 14:19:29 +0900

    날짜 표현방식 수정

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var dateFormat = require('dateformat');
 var fs = require('fs');
+var qs = require('querystring');
 var template = require('./template.js');
 var logToUML = require('./lib/logToPlantuml.js');
 var plantumlEncoder = require('plantuml-encoder');
@@ -17,7 +18,7 @@ var storage = multer.diskStorage({
     cb(null, 'data/logfiles')
   },
   filename: function (req, file, cb){
-    cb(null, Date.now() + '-' + file.originalname);
+    cb(null, dateFormat(Date.now(),"yyyymmddhhmmss") + '-' + file.originalname);
   }
 });
 
@@ -30,23 +31,70 @@ app.get('/', function(request, response){
     var title = "Packet Drawer";
     var subTitle = "LogFile List"
     var list = template.list(filelist);
-    var html = template.HTML(title, subTitle,
-      list,
+    var html = template.HTML(title, "",
+      `<p class="lead ml-2">${subTitle}</p>`+list,
       `
       <form action="/data/logfiles/uploads", method="POST", enctype="multipart/form-data">
         <div class="form-group">
-          <label> <p class="lead ml-2">Select Logfiles<p></label>
+          <label> <p class="lead ml-2">Upload Logfiles<p></label>
           <input type="file" class="form-control-file" name="userfile" multiple>
         </div>
         <div class="form-group">
           <button type="submit" class="btn btn-primary">Uploads</button>
         </div>        
       </form>
-      `, "");
+      `,
+      `
+      <form action="/parse/direct", method="POST">
+      <div class="form-group">
+        <label> <p class="lead ml-2">Direct Log Parse<p></label>
+        <textarea rows="50" cols="50" name="description" class="form-control-file"></textarea>
+      </div>
+      <div class="form-group">
+        <button type="submit" class="btn btn-primary">Parse!</button>
+      </div>        
+    </form>
+      `);
 
       response.send(html);
   });
 });
+
+app.get('/parse/direct', function(req, res){
+  var body = '';
+  
+  req.on('data', function(data){
+    body += data;
+  });
+
+  req.on('end', function(){
+
+    var post = qs.parse(body);
+    logToUML.parseToUML_String(post.description, ignoreMessages, function(err, result){
+      if (err !== null){
+        console.log(err);
+      }
+
+      var encoded = plantumlEncoder.encode(result);
+      var url = 'http://www.plantuml.com/plantuml/svg/' + encoded;
+
+      var prettyHTML = template.HTML(
+        `Parse Result`, "", 
+      `
+      <form>
+        <textarea name="umlbody" placeholder="umlbody" rows="50" cols="50">${result}</textarea>
+      </form>
+      `, 
+      `
+      <img src="${url}">
+      `,
+      `<a class="btn btn-primary ml-3" href="/">Go To Home</a>`
+      );
+  
+      res.send(prettyHTML);
+    });
+  });
+})
 
 app.get('/parse/:logFileName', function(request, response){
     logToUML.parseToUML(`./data/logfiles/${request.params.logFileName}`, ignoreMessages, function(err, result){
